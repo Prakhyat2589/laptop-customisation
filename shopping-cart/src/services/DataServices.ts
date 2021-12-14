0781d63fdc274cfe1c43a87959b0ab9b05f9178d
Prakhyat2589 <prakhyat25@gmail.com>
2021-12-14 22:31:19 +0530

    updating fetchData function

--- shopping-cart/src/services/DataServices.ts
+++ shopping-cart/src/services/DataServices.ts
@@ -1,33 +1,31 @@
 import { useState, useEffect } from "react";
-const useFetchData = <Payload,>(
-    url: string,
+const useFetchData = <Payload>(
+  url: string
 ): {
-    data: Payload | null;
-    done: boolean;
-
-
+  data: Payload | null;
+  done: boolean;
 } => {
-    const [data, dataSet] = useState<Payload | null>(null);
-    const [done, doneSet] = useState(false);
+  const [data, dataSet] = useState<Payload | null>(null);
+  const [done, doneSet] = useState(false);
 
-    useEffect(() => {
-        try {
-            fetch(url)
-                .then((resp) => resp.json())
-                .then((d: Payload) => {
-                    dataSet(d);
-                    doneSet(true);
-                })
-        }
-        catch (e) {
-            console.log("Error:", e)
-        };
-    }, [url]);
-
-    return {
-        data,
-        done,
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(url);
+        const respJson = await res.json();
+        dataSet(respJson);
+        doneSet(true);
+      } catch (e) {
+        console.log("Error:", e);
+      }
     };
-}
+    getData();
+  }, [url]);
 
-export { useFetchData }
+  return {
+    data,
+    done,
+  };
+};
+
+export { useFetchData };
